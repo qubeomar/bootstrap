60fbc692d5ec7afd5d2e66dfc93f786f3f345754
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2017-02-02 10:15:20 -0800

    java opts de-quoted

--- login.sh
+++ login.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+eval $(docker-machine env default)
 docker login -e $(gcloud auth list | grep ACTIVE | grep qubeship.io | awk '{print $2}')  -u _token -p "$(gcloud auth print-access-token)" https://gcr.io
 
