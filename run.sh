7c1a33f6799e913066f768bb211e28fc918508c0
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2017-04-26 22:04:49 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,8 +12,4 @@ fi
 base_command="docker-compose"
 options="up -d --remove-orphans"
 files="-f docker-compose.yaml"
-
-if [ "$components" == "all" ]; then
-	files="$files -f docker-compose-qube-extended.yaml"
-fi
 $base_command $files $options $services
